1347c4f0b4756e68803c4eaedf2890794825a6cf
TonyRegy <tonyregy@gmail.com>
2020-06-10 13:58:43 +0530

    ADDED:Functions for gambling result and stake limit

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -9,3 +9,41 @@ PT=0
 GT=0
 echo "stake per day =100$"
 echo "per bet=1$"
+
+function gamblingLimit(){
+echo "enter at what percentage of win/lose should you stop gambling"
+read limit
+limitValue=$(echo "scale=2;$limit / 100"|bc);echo
+b=$(echo "$limitValue * 100"|bc);
+int=${b%\.*}
+upperLimit=$(( $STAKE + $int ))
+lowerLimit=$(( $STAKE - $int ))
+}
+function gambling(){
+gamblingLimit
+while [[ $STAKE -le $upperLimit && $LOSS -le $lowerLimit ]]
+do
+        c=$((RANDOM%2))
+if [ $c -eq 0 ]
+then
+        echo "winner"
+        STAKE=$(($STAKE + 1))
+        u=$(($STAKE - 100))
+        array[((M++))]=$u
+        ((WON_COUNT++))
+else
+        echo "you lose"
+        LOSS=$(($LOSS + 1))
+        array1[((N++))]=$LOSS
+        ((LOSE_COUNT++))
+fi
+done
+gain=$(IFS=+; echo "$((${array[*]}))")
+loss=$(IFS=+; echo "$((${array1[*]}))")
+array3[((PT++))]=$gain
+array4[((GT++))]=$loss
+M=0
+N=0
+unset array
+unset array1
+}
